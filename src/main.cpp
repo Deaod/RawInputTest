616435dcf35df6c3b8abab33a4bc089cf2dbc26d
Lukas Bagaric <lukas.bagaric@gmail.com>
2018-05-21 16:43:54 +0200

    Add GLM

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,13 @@
 #include <vulkan/vulkan.h>
 #include "vulkan_tools.hpp"
 
+#pragma warning(push)
+#pragma warning(disable : 4201)
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#pragma warning(pop)
 
 LRESULT CALLBACK input_wndproc(HWND, UINT, WPARAM, LPARAM);
 
@@ -53,7 +56,7 @@ void input_thread_fn() {
 
         VK_ON_FAIL_RETURN_VOID(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
     }
-    
+
     VkPhysicalDevice physicalDevice;
     {
         u32 gpuCount = 0;
@@ -786,25 +789,6 @@ void input_thread_fn() {
         u32 count;
     } indices;
 
-    // Setup vertices
-    std::vector<Vertex> vertexBuffer = {
-        { {  1.0f,  1.0f, 0.0f }, { 1.0f, 0.0f, 0.0f } },
-        { { -1.0f,  1.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
-        { {  0.0f, -1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } }
-    };
-    u32 vertexBufferSize = u32(vertexBuffer.size() * sizeof(Vertex));
-
-    // Setup indices
-    std::vector<u32> indexBuffer = { 0, 1, 2 };
-    indices.count = u32(indexBuffer.size());
-    u32 indexBufferSize = u32(indices.count * sizeof(u32));
-
-    VkMemoryAllocateInfo memAlloc = {};
-    memAlloc.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    VkMemoryRequirements memReqs;
-
-    void *data;
-
     // Static data like vertex and index buffer should be stored on the device memory 
     // for optimal (and fastest) access by the GPU
     //
@@ -826,88 +810,115 @@ void input_thread_fn() {
         StagingBuffer indices;
     } stagingBuffers;
 
-    // Vertex buffer
-    VkBufferCreateInfo vertexBufferInfo = {};
-    vertexBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    vertexBufferInfo.size = vertexBufferSize;
-    // Buffer is used as the copy source
-    vertexBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-    // Create a host-visible buffer to copy the vertex data to (staging buffer)
-    VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &vertexBufferInfo, nullptr, &stagingBuffers.vertices.buffer));
-    vkGetBufferMemoryRequirements(device, stagingBuffers.vertices.buffer, &memReqs);
-    memAlloc.allocationSize = memReqs.size;
-    // Request a host visible memory type that can be used to copy our data do
-    // Also request it to be coherent, so that writes are visible to the GPU right after unmapping the buffer
-    memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-    VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &stagingBuffers.vertices.memory));
-    // Map and copy
-    VK_ON_FAIL_RETURN_VOID(vkMapMemory(device, stagingBuffers.vertices.memory, 0, memAlloc.allocationSize, 0, &data));
-    memcpy(data, vertexBuffer.data(), vertexBufferSize);
-    vkUnmapMemory(device, stagingBuffers.vertices.memory);
-    VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, stagingBuffers.vertices.buffer, stagingBuffers.vertices.memory, 0));
+    {
+        // Setup vertices
+        std::vector<Vertex> vertexBuffer = {
+            { {  1.0f,  1.0f, 0.0f }, { 1.0f, 0.0f, 0.0f } },
+            { { -1.0f,  1.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
+            { {  0.0f, -1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } }
+        };
+        u32 vertexBufferSize = u32(vertexBuffer.size() * sizeof(Vertex));
 
-    // Create a device local buffer to which the (host local) vertex data will be copied and which will be used for rendering
-    vertexBufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-    VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &vertexBufferInfo, nullptr, &vertices.buffer));
-    vkGetBufferMemoryRequirements(device, vertices.buffer, &memReqs);
-    memAlloc.allocationSize = memReqs.size;
-    memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-    VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &vertices.memory));
-    VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, vertices.buffer, vertices.memory, 0));
+        // Setup indices
+        std::vector<u32> indexBuffer = { 0, 1, 2 };
+        indices.count = u32(indexBuffer.size());
+        u32 indexBufferSize = u32(indices.count * sizeof(u32));
 
-    // Index buffer
-    VkBufferCreateInfo indexbufferInfo = {};
-    indexbufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    indexbufferInfo.size = indexBufferSize;
-    indexbufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-    // Copy index data to a buffer visible to the host (staging buffer)
-    VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &indexbufferInfo, nullptr, &stagingBuffers.indices.buffer));
-    vkGetBufferMemoryRequirements(device, stagingBuffers.indices.buffer, &memReqs);
-    memAlloc.allocationSize = memReqs.size;
-    memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-    VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &stagingBuffers.indices.memory));
-    VK_ON_FAIL_RETURN_VOID(vkMapMemory(device, stagingBuffers.indices.memory, 0, indexBufferSize, 0, &data));
-    memcpy(data, indexBuffer.data(), indexBufferSize);
-    vkUnmapMemory(device, stagingBuffers.indices.memory);
-    VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, stagingBuffers.indices.buffer, stagingBuffers.indices.memory, 0));
+        VkMemoryAllocateInfo memAlloc = {};
+        memAlloc.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        VkMemoryRequirements memReqs;
 
-    // Create destination buffer with device only visibility
-    indexbufferInfo.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-    VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &indexbufferInfo, nullptr, &indices.buffer));
-    vkGetBufferMemoryRequirements(device, indices.buffer, &memReqs);
-    memAlloc.allocationSize = memReqs.size;
-    memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-    VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &indices.memory));
-    VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, indices.buffer, indices.memory, 0));
+        void *data;
 
-    VkCommandBufferBeginInfo cmdBufferBeginInfo = {};
-    cmdBufferBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-    cmdBufferBeginInfo.pNext = nullptr;
+        // Vertex buffer
+        VkBufferCreateInfo vertexBufferInfo = {};
+        vertexBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        vertexBufferInfo.size = vertexBufferSize;
+        // Buffer is used as the copy source
+        vertexBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+        // Create a host-visible buffer to copy the vertex data to (staging buffer)
+        VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &vertexBufferInfo, nullptr, &stagingBuffers.vertices.buffer));
+        vkGetBufferMemoryRequirements(device, stagingBuffers.vertices.buffer, &memReqs);
+        memAlloc.allocationSize = memReqs.size;
+        // Request a host visible memory type that can be used to copy our data do
+        // Also request it to be coherent, so that writes are visible to the GPU right after unmapping the buffer
+        memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+        VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &stagingBuffers.vertices.memory));
+        // Map and copy
+        VK_ON_FAIL_RETURN_VOID(vkMapMemory(device, stagingBuffers.vertices.memory, 0, memAlloc.allocationSize, 0, &data));
+        memcpy(data, vertexBuffer.data(), vertexBufferSize);
+        vkUnmapMemory(device, stagingBuffers.vertices.memory);
+        VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, stagingBuffers.vertices.buffer, stagingBuffers.vertices.memory, 0));
 
-    // Buffer copies have to be submitted to a queue, so we need a command buffer for them
-    // Note: Some devices offer a dedicated transfer queue (with only the transfer bit set) that may be faster when doing lots of copies
-    VkCommandBuffer copyCmd = getCommandBuffer(true);
-    ON_FAIL_RETURN_VOID(copyCmd != VK_NULL_HANDLE);
+        // Create a device local buffer to which the (host local) vertex data will be copied and which will be used for rendering
+        vertexBufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+        VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &vertexBufferInfo, nullptr, &vertices.buffer));
+        vkGetBufferMemoryRequirements(device, vertices.buffer, &memReqs);
+        memAlloc.allocationSize = memReqs.size;
+        memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+        VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &vertices.memory));
+        VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, vertices.buffer, vertices.memory, 0));
 
-    // Put buffer region copies into command buffer
-    VkBufferCopy copyRegion = {};
+        // Index buffer
+        VkBufferCreateInfo indexbufferInfo = {};
+        indexbufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        indexbufferInfo.size = indexBufferSize;
+        indexbufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+        // Copy index data to a buffer visible to the host (staging buffer)
+        VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &indexbufferInfo, nullptr, &stagingBuffers.indices.buffer));
+        vkGetBufferMemoryRequirements(device, stagingBuffers.indices.buffer, &memReqs);
+        memAlloc.allocationSize = memReqs.size;
+        memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+        VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &stagingBuffers.indices.memory));
+        VK_ON_FAIL_RETURN_VOID(vkMapMemory(device, stagingBuffers.indices.memory, 0, indexBufferSize, 0, &data));
+        memcpy(data, indexBuffer.data(), indexBufferSize);
+        vkUnmapMemory(device, stagingBuffers.indices.memory);
+        VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, stagingBuffers.indices.buffer, stagingBuffers.indices.memory, 0));
 
-    // Vertex buffer
-    copyRegion.size = vertexBufferSize;
-    vkCmdCopyBuffer(copyCmd, stagingBuffers.vertices.buffer, vertices.buffer, 1, &copyRegion);
-    // Index buffer
-    copyRegion.size = indexBufferSize;
-    vkCmdCopyBuffer(copyCmd, stagingBuffers.indices.buffer, indices.buffer, 1, &copyRegion);
+        // Create destination buffer with device only visibility
+        indexbufferInfo.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+        VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &indexbufferInfo, nullptr, &indices.buffer));
+        vkGetBufferMemoryRequirements(device, indices.buffer, &memReqs);
+        memAlloc.allocationSize = memReqs.size;
+        memAlloc.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+        VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &memAlloc, nullptr, &indices.memory));
+        VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, indices.buffer, indices.memory, 0));
 
-    // Flushing the command buffer will also submit it to the queue and uses a fence to ensure that all commands have been executed before returning
-    VK_ON_FAIL_RETURN_VOID(flushCommandBuffer(copyCmd));
+        VkCommandBufferBeginInfo cmdBufferBeginInfo = {};
+        cmdBufferBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        cmdBufferBeginInfo.pNext = nullptr;
 
-    // Destroy staging buffers
-    // Note: Staging buffer must not be deleted before the copies have been submitted and executed
-    vkDestroyBuffer(device, stagingBuffers.vertices.buffer, nullptr);
-    vkFreeMemory(device, stagingBuffers.vertices.memory, nullptr);
-    vkDestroyBuffer(device, stagingBuffers.indices.buffer, nullptr);
-    vkFreeMemory(device, stagingBuffers.indices.memory, nullptr);
+        // Buffer copies have to be submitted to a queue, so we need a command buffer for them
+        // Note: Some devices offer a dedicated transfer queue (with only the transfer bit set) that may be faster when doing lots of copies
+        VkCommandBuffer copyCmd = getCommandBuffer(true);
+        ON_FAIL_RETURN_VOID(copyCmd != VK_NULL_HANDLE);
+
+        // Put buffer region copies into command buffer
+        VkBufferCopy copyRegion = {};
+
+        // Vertex buffer
+        copyRegion.size = vertexBufferSize;
+        vkCmdCopyBuffer(copyCmd, stagingBuffers.vertices.buffer, vertices.buffer, 1, &copyRegion);
+        // Index buffer
+        copyRegion.size = indexBufferSize;
+        vkCmdCopyBuffer(copyCmd, stagingBuffers.indices.buffer, indices.buffer, 1, &copyRegion);
+
+        // Flushing the command buffer will also submit it to the queue and uses a fence to ensure that all commands have been executed before returning
+        VK_ON_FAIL_RETURN_VOID(flushCommandBuffer(copyCmd));
+
+        // Destroy staging buffers
+        // Note: Staging buffer must not be deleted before the copies have been submitted and executed
+        vkDestroyBuffer(device, stagingBuffers.vertices.buffer, nullptr);
+        vkFreeMemory(device, stagingBuffers.vertices.memory, nullptr);
+        vkDestroyBuffer(device, stagingBuffers.indices.buffer, nullptr);
+        vkFreeMemory(device, stagingBuffers.indices.memory, nullptr);
+        }
+    // Uniform buffer block object
+    struct {
+        VkDeviceMemory memory;
+        VkBuffer buffer;
+        VkDescriptorBufferInfo descriptor;
+    }  uniformBufferVS;
 
     // For simplicity we use the same uniform block layout as in the shader:
     //
@@ -926,37 +937,42 @@ void input_thread_fn() {
         glm::mat4 viewMatrix;
     } uboVS;
 
-    // Prepare and initialize a uniform buffer block containing shader uniforms
-    // Single uniforms like in OpenGL are no longer present in Vulkan. All Shader uniforms are passed via uniform buffer blocks
-    VkMemoryRequirements memReqs;
+    glm::vec3 rotation = glm::vec3();
+    float zoom = -2.5f;
 
-    // Vertex shader uniform buffer block
-    VkBufferCreateInfo bufferInfo = {};
-    VkMemoryAllocateInfo allocInfo = {};
-    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    allocInfo.pNext = nullptr;
-    allocInfo.allocationSize = 0;
-    allocInfo.memoryTypeIndex = 0;
+    {
+        // Prepare and initialize a uniform buffer block containing shader uniforms
+        // Single uniforms like in OpenGL are no longer present in Vulkan. All Shader uniforms are passed via uniform buffer blocks
+        VkMemoryRequirements memReqs;
 
-    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    bufferInfo.size = sizeof(uboVS);
-    // This buffer will be used as a uniform buffer
-    bufferInfo.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+        // Vertex shader uniform buffer block
+        VkBufferCreateInfo bufferInfo = {};
+        VkMemoryAllocateInfo allocInfo = {};
+        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocInfo.pNext = nullptr;
+        allocInfo.allocationSize = 0;
+        allocInfo.memoryTypeIndex = 0;
 
-    // Create a new buffer
-    VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &bufferInfo, nullptr, &uniformBufferVS.buffer));
-    // Get memory requirements including size, alignment and memory type 
-    vkGetBufferMemoryRequirements(device, uniformBufferVS.buffer, &memReqs);
-    allocInfo.allocationSize = memReqs.size;
-    // Get the memory type index that supports host visibile memory access
-    // Most implementations offer multiple memory types and selecting the correct one to allocate memory from is crucial
-    // We also want the buffer to be host coherent so we don't have to flush (or sync after every update.
-    // Note: This may affect performance so you might not want to do this in a real world application that updates buffers on a regular base
-    allocInfo.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-    // Allocate memory for the uniform buffer
-    VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &allocInfo, nullptr, &(uniformBufferVS.memory)));
-    // Bind memory to buffer
-    VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, uniformBufferVS.buffer, uniformBufferVS.memory, 0));
+        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        bufferInfo.size = sizeof(uboVS);
+        // This buffer will be used as a uniform buffer
+        bufferInfo.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+
+        // Create a new buffer
+        VK_ON_FAIL_RETURN_VOID(vkCreateBuffer(device, &bufferInfo, nullptr, &uniformBufferVS.buffer));
+        // Get memory requirements including size, alignment and memory type 
+        vkGetBufferMemoryRequirements(device, uniformBufferVS.buffer, &memReqs);
+        allocInfo.allocationSize = memReqs.size;
+        // Get the memory type index that supports host visibile memory access
+        // Most implementations offer multiple memory types and selecting the correct one to allocate memory from is crucial
+        // We also want the buffer to be host coherent so we don't have to flush (or sync after every update.
+        // Note: This may affect performance so you might not want to do this in a real world application that updates buffers on a regular base
+        allocInfo.memoryTypeIndex = getMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+        // Allocate memory for the uniform buffer
+        VK_ON_FAIL_RETURN_VOID(vkAllocateMemory(device, &allocInfo, nullptr, &(uniformBufferVS.memory)));
+        // Bind memory to buffer
+        VK_ON_FAIL_RETURN_VOID(vkBindBufferMemory(device, uniformBufferVS.buffer, uniformBufferVS.memory, 0));
+    }
 
     // Store information in the uniform's descriptor that is used by the descriptor set
     uniformBufferVS.descriptor.buffer = uniformBufferVS.buffer;
