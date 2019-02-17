ebcbdb5635151dc35f94d58708888f744fc233b2
Lukas Bagaric <deaeod@gmail.com>
2019-02-17 02:33:27 +0100

    Add scope_guard Remove unused file Use scope_guard to bring exception-safety to spsc_ring_buffer Allow  individual entries in spsc_ring_buffer to be aligned on cache lines.

--- src/spsc_ring_buffer.hpp
+++ src/spsc_ring_buffer.hpp
@@ -2,13 +2,26 @@
 
 #include <atomic>
 #include <cstddef>
+#include <type_traits>
 #include "compile_time_utilities.hpp"
+#include "scope_guard.hpp"
 
-template<int _buffer_size_log2, int _align_log2 = 6>
+template<
+    int _buffer_size_log2,
+    int _align_log2 = 6,
+    typename _difference_type = ptrdiff_t,
+    int _content_align_log2 = ctu::log2_v<sizeof(_difference_type)>
+>
 struct alignas(size_t(1) << _align_log2) spsc_ring_buffer {
+    using difference_type = _difference_type;
     static const auto size = size_t(1) << _buffer_size_log2;
     static const auto mask = ctu::bit_mask_v<size_t, _buffer_size_log2>;
     static const auto align = size_t(1) << _align_log2;
+    static const auto content_align_log2 = _content_align_log2;
+
+    static_assert(_buffer_size_log2 < ctu::bits_of<difference_type>);
+    static_assert(std::is_signed_v<difference_type>);
+    static_assert(content_align_log2 >= ctu::log2(sizeof(difference_type)));
 
     template<typename cbtype>
     bool produce(size_t length, cbtype callback) noexcept(noexcept(callback(static_cast<void*>(nullptr)))) {
@@ -18,23 +31,23 @@ struct alignas(size_t(1) << _align_log2) spsc_ring_buffer {
         auto consume_pos = _consume_pos.load(std::memory_order_acquire);
         auto produce_pos = _produce_pos.load(std::memory_order_acquire);
 
-        auto rounded_length = ctu::round_up_bits(length, ctu::log2_v<sizeof(ptrdiff_t)>);
+        auto rounded_length = ctu::round_up_bits(length + sizeof(difference_type), content_align_log2);
 
-        if ((produce_pos - consume_pos) > (size - (rounded_length + sizeof(ptrdiff_t))))
+        if ((produce_pos - consume_pos) > (size - rounded_length))
             return false;
 
         auto wrap_distance = size - (produce_pos & mask);
-        if (wrap_distance < (rounded_length + sizeof(ptrdiff_t))) {
-            if ((produce_pos + wrap_distance - consume_pos) > (size - (rounded_length + sizeof(ptrdiff_t))))
+        if (wrap_distance < rounded_length) {
+            if ((produce_pos + wrap_distance - consume_pos) > (size - rounded_length))
                 return false;
 
-            new (_buffer + (produce_pos & mask)) ptrdiff_t(-ptrdiff_t(wrap_distance));
+            new (_buffer + (produce_pos & mask)) difference_type(-difference_type(wrap_distance));
             produce_pos += wrap_distance;
         }
 
-        new (_buffer + (produce_pos & mask)) ptrdiff_t(length);
-        if (callback(static_cast<void*>(_buffer + (produce_pos & mask) + sizeof(ptrdiff_t)))) {
-            _produce_pos.store(produce_pos + (rounded_length + sizeof(ptrdiff_t)), std::memory_order_release);
+        new (_buffer + (produce_pos & mask)) difference_type(length);
+        if (callback(static_cast<void*>(_buffer + (produce_pos & mask) + sizeof(difference_type)))) {
+            _produce_pos.store(produce_pos + rounded_length, std::memory_order_release);
             return true;
         }
 
@@ -42,14 +55,14 @@ struct alignas(size_t(1) << _align_log2) spsc_ring_buffer {
     }
 
     template<typename cbtype>
-    bool consume(cbtype callback) noexcept(noexcept(callback(static_cast<void*>(nullptr), ptrdiff_t(0)))) {
+    bool consume(cbtype callback) noexcept(noexcept(callback(static_cast<void*>(nullptr), difference_type(0)))) {
         auto consume_pos = _consume_pos.load(std::memory_order_acquire);
         auto produce_pos = _produce_pos.load(std::memory_order_acquire);
 
         if (produce_pos == consume_pos)
             return false;
 
-        ptrdiff_t length;
+        difference_type length;
         memcpy(&length, _buffer + (consume_pos & mask), sizeof(length));
         
         if (length < 0) {
@@ -57,9 +70,9 @@ struct alignas(size_t(1) << _align_log2) spsc_ring_buffer {
             memcpy(&length, _buffer + (consume_pos & mask), sizeof(length));
         }
 
-        if (callback(static_cast<void*>(_buffer + (consume_pos & mask) + sizeof(ptrdiff_t)), length)) {
-            auto rounded_length = ctu::round_up_bits(length, ctu::log2_v<sizeof(ptrdiff_t)>);
-            _consume_pos.store(consume_pos + (rounded_length + sizeof(ptrdiff_t)), std::memory_order_release);
+        if (callback(static_cast<void*>(_buffer + (consume_pos & mask) + sizeof(difference_type)), length)) {
+            auto rounded_length = ctu::round_up_bits(length + sizeof(difference_type), content_align_log2);
+            _consume_pos.store(consume_pos + rounded_length, std::memory_order_release);
             return true;
         }
 
@@ -68,16 +81,20 @@ struct alignas(size_t(1) << _align_log2) spsc_ring_buffer {
 
     // returns true if buffer is empty after this call
     template<typename cbtype>
-    bool consume_all(cbtype callback) noexcept(noexcept(callback(static_cast<void*>(nullptr), ptrdiff_t(0)))) {
+    bool consume_all(cbtype callback) noexcept(noexcept(callback(static_cast<void*>(nullptr), difference_type(0)))) {
         auto consume_pos = _consume_pos.load(std::memory_order_acquire);
         auto produce_pos = _produce_pos.load(std::memory_order_acquire);
 
         if (produce_pos == consume_pos)
             return true;
 
+        scope_guard g([this, &consume_pos]() {
+            _consume_pos.store(consume_pos, std::memory_order_release);
+        });
+
         while (consume_pos != produce_pos) {
             while (consume_pos != produce_pos) {
-                ptrdiff_t length;
+                difference_type length;
                 memcpy(&length, _buffer + (consume_pos & mask), sizeof(length));
 
                 if (length < 0) {
@@ -85,24 +102,17 @@ struct alignas(size_t(1) << _align_log2) spsc_ring_buffer {
                     memcpy(&length, _buffer + (consume_pos & mask), sizeof(length));
                 }
 
-                try {
-                    if (callback(static_cast<void*>(_buffer + (consume_pos & mask) + sizeof(ptrdiff_t)), length) == false) {
-                        _consume_pos.store(consume_pos, std::memory_order_release);
-                        return false;
-                    }
-                } catch (...) {
-                    _consume_pos.store(consume_pos, std::memory_order_release);
-                    throw;
+                if (callback(static_cast<void*>(_buffer + (consume_pos & mask) + sizeof(difference_type)), length) == false) {
+                    return false;
                 }
 
-                auto rounded_length = ctu::round_up_bits(length, ctu::log2_v<sizeof(ptrdiff_t)>);
-                consume_pos += (rounded_length + sizeof(ptrdiff_t));
+                auto rounded_length = ctu::round_up_bits(length + sizeof(difference_type), content_align_log2);
+                consume_pos += rounded_length;
             }
 
             produce_pos = _produce_pos.load(std::memory_order_acquire);
         }
 
-        _consume_pos.store(consume_pos, std::memory_order_release);
         return (consume_pos == produce_pos);
     }
 
